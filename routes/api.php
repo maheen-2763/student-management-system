1ce0c0d44d76daaca65f91de33f52bebb42ae99e
Mohammed Maheen Afzal <>
2026-04-21 04:12:37 +0530

    new naming

--- routes/api.php
+++ routes/api.php
@@ -16,7 +16,7 @@ Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 
 
-Route::middleware(['auth:sanctum', 'throttle:api'])->group(function () {
+Route::middleware(['auth:sanctum'])->group(function () {
 
     Route::post('/logout', [AuthController::class, 'logout']);
 
