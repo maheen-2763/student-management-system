5a7dec02fc648856076433b5bc4006e197c8910e
Mohammed Maheen Afzal <>
2026-04-21 13:50:46 +0530

    throttling added

--- routes/api.php
+++ routes/api.php
@@ -16,7 +16,7 @@ Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 
 
-Route::middleware(['auth:sanctum'])->group(function () {
+Route::middleware(['auth:sanctum', 'throttle:60,1'])->group(function () {
 
     Route::post('/logout', [AuthController::class, 'logout']);
 
